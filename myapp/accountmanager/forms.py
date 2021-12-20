54e8808d1ee92887e2b06a437e23e9583b42ef7b
Ata Atasoy <h.ataatasoy@gmail.com>
2021-12-20 17:41:51 +0300

    fix login typos & create register.css

--- myapp/accountmanager/forms.py
+++ myapp/accountmanager/forms.py
@@ -6,24 +6,3 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ('username', 'email', 'password1', 'password2')
-
-    def __init__(self, *args, **kwargs) -> None:
-        kwargs.setdefault('label_suffix', '')
-        super().__init__(*args, **kwargs)
-
-        self.fields['username'].label = "Username"
-        self.fields['email'].label = "Email"
-        self.fields['password1'].label = "Password"
-        self.fields['password2'].label = "Confirm Password"
-
-        self.fields['username'].widget.attrs.update(
-            {'class': 'myfieldclass form-control'})
-        self.fields['email'].widget.attrs.update(
-            {'class': 'myfieldclass form-control'})
-        self.fields['password1'].widget.attrs.update(
-            {'class': 'myfieldclass form-control'})
-        self.fields['password2'].widget.attrs.update(
-            {'class': 'myfieldclass form-control'})
-
-        for text in ['username', 'password1', 'password2']:
-            self.fields[text].help_text = None
