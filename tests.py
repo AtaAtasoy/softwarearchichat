b2b79fff77a4bb5e0b9e6b5bef9f15358b83f616
Ata Atasoy <h.ataatasoy@gmail.com>
2021-12-21 15:31:01 +0300

    add print messages to the test

--- tests.py
+++ tests.py
@@ -36,8 +36,11 @@ password.send_keys(valid_password)
 password2.send_keys(valid_password)
 register_btn.click()
 WebDriverWait(browser, timeout=10).until(EC.title_is("Chat Rooms"))
+print("REGISTERED ", valid_username, " SUCCESSFULLY")
 browser.get("http://localhost:8000/accounts/logout")
 assert browser.title == "Login Page"
+print("LOGGED OUT ", valid_username, " SUCCESSFULLY")
+
 
 browser.get("http://localhost:8000/accounts/register")
 assert browser.title == "Register Page"
@@ -54,8 +57,12 @@ password.send_keys(valid_password2)
 password2.send_keys(valid_password2)
 register_btn.click()
 WebDriverWait(browser, timeout=10).until(EC.title_is("Chat Rooms"))
+print("REGISTERED ", valid_username2, " SUCCESSFULLY")
+
 browser.get("http://localhost:8000/accounts/logout")
 assert browser.title == "Login Page"
+print("LOGGED OUT ", valid_username2, " SUCCESSFULLY")
+
 
 username = browser.find_element(By.XPATH, "/html/body/div/div[2]/div/div[2]/form/input[2]")
 password = browser.find_element(By.XPATH, "/html/body/div/div[2]/div/div[2]/form/input[3]")
@@ -67,6 +74,7 @@ submit.click()
 
 WebDriverWait(browser, timeout=10).until(EC.title_is("Chat Rooms"))
 assert browser.title == "Chat Rooms"
+print("LOGGED ", valid_username, " IN SUCCESSFULLY")
 
 WebDriverWait(browser, timeout=10).until(EC.presence_of_all_elements_located)
 
@@ -76,14 +84,21 @@ contact.click()
 WebDriverWait(browser, timeout=10).until(EC.presence_of_all_elements_located)
 chat_room_title = browser.find_element(By.XPATH, "/html/body/div/div[2]/div[2]/div[2]/span").text
 assert contact_name == chat_room_title == valid_username2
+print("OPENED CHAT WITH ", valid_username2, " SUCCESSFULLY")
+
 
 message_box = browser.find_element(By.XPATH, '//*[@id="message_input"]')
 send_button = browser.find_element(By.XPATH, '//*[@id="chat-message-submit"]')
 message_box.send_keys("This is a test message")
 send_button.click()
+message_table = browser.find_element(By.XPATH, '/html/body/div/div[2]/div[2]/div[2]/div/div[1]')
+browser.implicitly_wait(5)
+assert "This is a test message" in message_table.text
+print("SENT MESSAGE SUCCESSFULLY")
 
 browser.get("http://localhost:8000/accounts/logout")
 WebDriverWait(browser, timeout=10).until(EC.presence_of_all_elements_located)
+print("LOGGED OUT ", valid_username, " SUCCESSFULLY")
 
 username = browser.find_element(By.XPATH, "/html/body/div/div[2]/div/div[2]/form/input[2]")
 password = browser.find_element(By.XPATH, "/html/body/div/div[2]/div/div[2]/form/input[3]")
@@ -94,6 +109,7 @@ password.send_keys(valid_password2)
 submit.click()
 WebDriverWait(browser, timeout=10).until(EC.title_is("Chat Rooms"))
 assert browser.title == "Chat Rooms"
+print("LOGGED ", valid_username2, " IN SUCCESSFULLY")
 
 WebDriverWait(browser, timeout=10).until(EC.presence_of_all_elements_located)
 contact = browser.find_element(By.LINK_TEXT, valid_username)
@@ -101,9 +117,11 @@ contact_name = contact.text
 contact.click()
 chat_room_title = browser.find_element(By.XPATH, "/html/body/div/div[2]/div[2]/div[2]/span").text
 assert chat_room_title == valid_username == contact_name
+print("OPENED CHAT WITH ", valid_username, " SUCCESSFULLY")
+
 
 message_table = browser.find_element(By.XPATH, '/html/body/div/div[2]/div[2]/div[2]/div/div[1]')
 assert "This is a test message" in message_table.text
+print("RECEIVED THE MESSAGE ", valid_username, " SUCCESSFULLY")
 
 browser.close()
-
